e360f658f70df7b30a2e98a473a844cfe7c0173a
Maxi <maxitiezzi@gmail.com>
2023-05-15 16:29:16 -0300

    ss

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -11,7 +11,7 @@ export class DashboardController {
 
     @Get()
     @ApiTags('dashboard')
-    @ApiOperation({ summary: 'Devuelve un array con todos los dashboards', description: 'Devuelve un array con todos los dashboards' })
+    @ApiOperation({ summary: 'Devuelve un array con todos los dashboards', description: 'Devuelve un array con todos los dashboards pullll' })
     @ApiResponse({ status: 200, type: Dashboard, })
     @ApiResponse({ status: 404, type: Error, })
     getAll() {
